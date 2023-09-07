4b6f6612ed89f617f2958057271e54c6e26ec205
BajanovVladimir <bajanov_v_s@mail.ru>
2023-09-07 16:51:53 +0400

    Fixed changeset-created-user.sql и changelog.xml

--- src/main/resources/db/changeset-created-user.sql
+++ src/main/resources/db/changeset-created-user.sql
@@ -1,10 +1,7 @@
 insert into users (login, password) values ('user1', 'password1');
 insert into roles (name) values ('Role_LESSOR');
 insert into roles (name) values ('Role_RENTER');
-<<<<<<< HEAD
 insert into users_roles (user_id,role_id) values(1,1);
 insert into users (login, password) values ('user2', 'password2');
 insert into users_roles (user_id,role_id) values(1,2);
-=======
 insert into users_roles (user_id,role_id) values(1,1);
->>>>>>> c6039f15535ff6412cea36a5d92ed4f314d5a5bb
