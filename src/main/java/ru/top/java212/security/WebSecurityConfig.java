716f8fc40e533eb9714aafa326adcdbe2e2ce1f0
BajanovVladimir <bajanov_v_s@mail.ru>
2023-09-11 23:36:44 +0400

    started creating the home page, registration and login page

---
+++ src/main/java/ru/top/java212/security/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package ru.top.java212.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.top.java212.model.Role;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private  UserDetailsService userDetailsService;
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.userDetailsService(userDetailsService)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .permitAll()
+                        .successForwardUrl("/menu")
+                        .failureForwardUrl("/login_error")
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .permitAll()
+                        .logoutSuccessUrl("/login")
+                );
+        return http.build();
+
+    }
+
+
+
+}
