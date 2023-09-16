f242bff5fe879a8155c23d98f48810e69ea4bc99
BajanovVladimir <bajanov_v_s@mail.ru>
2023-09-16 23:42:28 +0400

    created login, registration and error pages

--- src/main/java/ru/top/java212/security/WebSecurityConfig.java
+++ src/main/java/ru/top/java212/security/WebSecurityConfig.java
@@ -3,13 +3,10 @@ package ru.top.java212.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
-import ru.top.java212.model.Role;
-
 
 @Configuration
 @EnableWebSecurity
